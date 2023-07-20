bc604a3cacbfaf89312d4f1c61ba38b8c4ed9373
Johan Callens <weylin030@protonmail.ch>
2023-07-20 15:33:10 +0200

    fixed active states issues

--- script.js
+++ script.js
@@ -1,37 +1,25 @@
 const ratingButtons = document.querySelectorAll(".rounded-full");
-const submitBtn = document.querySelector('.btn');
+const submitBtn = document.querySelector(".btn");
 const component = document.querySelector(".component");
 const modal = document.querySelector(".modal");
 const selected = document.getElementById("selected");
 
 //console.log(ratingButtons);
-ratingButtons.forEach((ratingBtn, index0) => {
-ratingBtn.addEventListener('click',() => {
-    //console.log(index0);
-    ratingButtons.forEach((ratingBtn, index1) => {
-    //console.log(index1);
-        if(index0 >= index1) {
-            ratingBtn.classList.add("active");
-        }else{
-            ratingBtn.classList.remove("active"); 
-        }
-    });
+ratingButtons.forEach((ratingBtn) => {
+  ratingBtn.addEventListener("click", () => {
+    ratingBtn.classList.add("active");
+    //console.log(ratingBtn.innerHTML)
+
 
     selected.innerHTML = ratingBtn.innerHTML;
-}); 
+  });
 });
-
-
-
 
 // modal default style ( not displayed)
 modal.style.display = "none";
 
-
 /*on click button function */
-submitBtn.addEventListener('click', () => {
-    component.style.display = "none";
-    modal.style.display="initial";
-    
+submitBtn.addEventListener("click", () => {
+  component.style.display = "none";
+  modal.style.display = "initial";
 });
-
